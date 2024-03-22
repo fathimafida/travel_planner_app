712ef6129fd069d381057d6d7d2a0d744015da30
Fathima Fida <fidafirozkattungal@gmail.com>
2024-03-22 20:42:41 +0530

    tabbar completed

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -1,20 +1,119 @@
 import 'package:flutter/material.dart';
 import 'package:travel_planner_app/common/helper.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
+
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            largeTextStyle("Discover", 25),
+            Row(
+              children: [
+                Container(child: Icon(Icons.menu)),
+                Spacer(),
+              ],
+            ),
+            Container(
+              child: largeTextStyle("Discover", 25),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                  labelPadding: EdgeInsets.only(left: 20, right: 20),
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  isScrollable: true,
+                  // indicator: CircleTabIndicator(color: Colors.black, radius: 4),
+                  tabs: [
+                    Tab(
+                      text: "Places",
+                    ),
+                    Tab(
+                      text: "Inspiration",
+                    ),
+                    Tab(
+                      text: "Emotions",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 20),
+                height: 300,
+                width: double.maxFinite,
+                child: TabBarView(controller: _tabController, children: [
+                  ListView.builder(
+                      itemCount: 3,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 10),
+                          height: 300,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        );
+                      }),
+                  Text("Inspiration"),
+                  Text("Emotions"),
+                ])),
           ],
         ),
       ),
     );
+  }
+}
+
+// ignore: must_be_immutable
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+  CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    // TODO: implement createBoxPainter
+
+    return _circlePointer(color: color, radius: radius);
+  }
+}
+
+class _circlePointer extends BoxPainter {
+  final Color color;
+  double radius;
+  _circlePointer({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    final Offset circleOffset =
+        offset + Offset(cfg.size!.width / 2 - radius / 2, cfg.size!.height - 5);
+    canvas.drawCircle(circleOffset, radius, _paint);
   }
 }
